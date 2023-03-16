3f8935e5e5a0d49fd3b1a0eb92830bc74a34f8df
Telperion <twice.invisible@gmail.com>
2023-03-16 12:09:06 -0700

    changing kernel application, etc.
    - I think convolving with the digest might have better sensitivity?
    - some math that didn't account for spectrogram with different size window/step
    - don't latch onto edges of fingerprint when looking for convolution response

--- nine-or-null/nine_or_null/__init__.py
+++ nine-or-null/nine_or_null/__init__.py
@@ -19,6 +19,10 @@ class BiasKernel(IntEnum):
     LOUDEST = 0
     RISING = 1
 
+class KernelTarget(IntEnum):
+    DIGEST = 0
+    ACCUMULATOR = 1
+
 
 def slugify(value, allow_unicode=False):
     """
@@ -47,7 +51,7 @@ def guess_paradigm(sync_bias):
         return 'unclear paradigm'
 
 
-def check_sync_bias(simfile_dir, plot_dir=None, show_intermediate_plots=False, kernel_type=BiasKernel.RISING):
+def check_sync_bias(simfile_dir, plot_dir=None, show_intermediate_plots=False, kernel_type=BiasKernel.RISING, kernel_target=KernelTarget.DIGEST):
     # Open simfile
     test_audio_path = None
     test_simfile_path = None
@@ -91,8 +95,8 @@ def check_sync_bias(simfile_dir, plot_dir=None, show_intermediate_plots=False, k
     # Create spectrogram from audio
     # https://stackoverflow.com/questions/44787437/how-to-convert-a-wav-file-to-a-spectrogram-in-python3
     # https://stackoverflow.com/questions/47954034/plotting-spectrogram-in-audio-analysis
-    window_ms = 5       # Window to calculate spectrogram over, ms
-    step_ms = 1         # Overlap between windows (effectively step size), ms
+    window_ms = 10      # Window to calculate spectrogram over, ms
+    step_ms = 0.2       # Overlap between windows (effectively step size), ms
     eps = 1e-9          # Epsilon for logarithms
 
     nperseg = int(audio.frame_rate * window_ms * 1e-3)              # number of samples per spectrogram segment
@@ -146,8 +150,12 @@ def check_sync_bias(simfile_dir, plot_dir=None, show_intermediate_plots=False, k
             # Too late
             break
 
-        t_s = max(0,              int(t / actual_step - fingerprint_size * 0.5))
-        t_f = min(times.shape[0], int(t / actual_step + fingerprint_size * 0.5))
+        # Because the spectrogram doesn't "start" until a full window is in view,
+        # it has an inherent offset that amounts to half a window.
+        spectrogram_offset = window_ms * 5e-4
+
+        t_s = max(0,              int((t - spectrogram_offset) / actual_step - fingerprint_size * 0.5))
+        t_f = min(times.shape[0], int((t - spectrogram_offset) / actual_step + fingerprint_size * 0.5))
         if (t_f - t_s != fingerprint_size):
             # Not enough data at this beat tbh
             continue
@@ -168,41 +176,48 @@ def check_sync_bias(simfile_dir, plot_dir=None, show_intermediate_plots=False, k
 
     if kernel_type == BiasKernel.LOUDEST:
         # Loudest point of attack
-        time_edge_kernel = [
+        time_edge_kernel = np.array([
             [1, 3, 10, 3, 1],
             [1, 3, 10, 3, 1],
             [1, 3, 10, 3, 1],
             [1, 3, 10, 3, 1],
             [1, 3, 10, 3, 1]
-        ]
-        time_edge_offset = 0.000
+        ])
     else:   # BiasKernel.RISING
         # Leading edge of attack
-        time_edge_kernel = [
-            [1, 3, 10, 0, -10, -3, -1],
-            [1, 3, 10, 0, -10, -3, -1],
-            [1, 3, 10, 0, -10, -3, -1],
-            [1, 3, 10, 0, -10, -3, -1],
-            [1, 3, 10, 0, -10, -3, -1]
-        ]
-        time_edge_offset = 0.003
+        time_edge_kernel = np.array([
+            [1, 3, 10, 30, 0, -30, -10, -3, -1],
+            [1, 3, 10, 30, 0, -30, -10, -3, -1],
+            [1, 3, 10, 30, 0, -30, -10, -3, -1],
+            [1, 3, 10, 30, 0, -30, -10, -3, -1],
+            [1, 3, 10, 30, 0, -30, -10, -3, -1]
+        ])
+    time_edge_offset = 0.002    # Why though
 
-    acc_edge = signal.convolve2d(acc, time_edge_kernel, mode='same', boundary='wrap')
-    # acc_edge = signal.convolve2d(digest, time_edge_kernel, mode='same', boundary='wrap')
+    digest_axis = np.arange(digest.shape[0])
 
-    # Flatten frequencies to a value that only depends on time
-    acc_edge_sum = np.sum(acc_edge, axis=0)
+    if kernel_target == KernelTarget.ACCUMULATOR:
+        post_kernel = signal.convolve2d(acc,    time_edge_kernel, mode='same', boundary='wrap')
+    else: # kernel_target == KernelTarget.DIGEST
+        post_kernel = signal.convolve2d(digest, time_edge_kernel, mode='same', boundary='wrap')
+    
+    # Flatten convolved fingerprint to a value that only depends on time
+    post_kernel_flat = np.sum(post_kernel, axis=0)
     fingerprint_times = np.arange(-fingerprint_size // 2, fingerprint_size // 2) * actual_step
+    fingerprint_times_ms = fingerprint_times * 1e3
 
     # Choose the highest response to the convolution as the downbeat attack
-    sync_bias = fingerprint_times[np.argmax(acc_edge_sum)] + time_edge_offset
+    edge_discard = time_edge_kernel.shape[1] // 2
+    sync_bias = fingerprint_times[np.argmax(post_kernel_flat[edge_discard:-edge_discard]) + edge_discard] + time_edge_offset
     probable_bias = guess_paradigm(sync_bias)
     # print(f'Sync bias: {sync_bias:0.3f} ({probable_bias})')
 
     # Set up visuals to show the user what's going on.
     plot_title = f'Sync fingerprint for {simfile_artist} - "{simfile_title}"\nDerived sync bias: {sync_bias:0.3f} ({probable_bias})'
     sanitized_title = slugify(simfile_title, allow_unicode=False)
-    time_ticks = np.arange(-fingerprint_sec * 1000, fingerprint_sec * 1000, 10)
+    time_ticks = np.arange(-fingerprint_sec * 500, fingerprint_sec * 501, 10)
+    frequency_line = np.ones(np.shape(frequencies)) * sync_bias * 1e3
+    beatindex_line = np.ones(np.shape(digest)[0]) * sync_bias * 1e3
     
     if plot_dir is not None:
         os.makedirs(plot_dir, exist_ok=True)
@@ -211,47 +226,53 @@ def check_sync_bias(simfile_dir, plot_dir=None, show_intermediate_plots=False, k
 
     # Accumulator in frequency domain
     fig = plt.figure(figsize=(6, 6))
-    plt.pcolormesh(fingerprint_times * 1e3, frequencies, acc)
+    plt.pcolormesh(fingerprint_times_ms, frequencies, acc)
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [msec]')
     plt.xticks(time_ticks)
-    plt.plot(np.ones(np.shape(frequencies)) * sync_bias * 1e3, frequencies, 'r-')
+    plt.plot(frequency_line, frequencies, 'r-')
     plt.title(plot_title)
     if show_intermediate_plots:
         plt.show()
     fig.savefig(os.path.join(plot_dir, f'bias-freqdomain-{sanitized_title}.png'))
     plt.close(fig)
 
-    # Accumulator in frequency domain after convolution
-    fig = plt.figure(figsize=(6, 6))
-    plt.pcolormesh(fingerprint_times * 1e3, frequencies, acc_edge)
-    plt.ylabel('Frequency [Hz]')
-    plt.xlabel('Time [msec]')
-    plt.xticks(time_ticks)
-    plt.plot(np.ones(np.shape(frequencies)) * sync_bias * 1e3, frequencies, 'r-')
-    plt.title(plot_title)
-    if show_intermediate_plots:
-        plt.show()
-    fig.savefig(os.path.join(plot_dir, f'bias-freqkernel-{sanitized_title}.png'))
-    plt.close(fig)
-
     # Digest in beat domain
     fig = plt.figure(figsize=(6, 6))
-    plt.pcolormesh(fingerprint_times * 1e3, np.arange(digest.shape[0]), digest)
+    plt.pcolormesh(fingerprint_times_ms, digest_axis, digest)
     plt.clim(np.percentile(digest[:], 10), np.percentile(digest[:], 90))
     plt.ylabel('Beat Index')
     plt.xlabel('Time [msec]')
     plt.xticks(time_ticks)
-    plt.plot(np.ones(np.shape(digest)[0]) * sync_bias * 1e3, np.arange(digest.shape[0]), 'r-')
+    plt.plot(beatindex_line, digest_axis, 'r-')
     plt.title(plot_title)
     if show_intermediate_plots:
         plt.show()
     fig.savefig(os.path.join(plot_dir, f'bias-beatdigest-{sanitized_title}.png'))
     plt.close(fig)
 
+    # Post-convolution plot
+    fig = plt.figure(figsize=(6, 6))
+    if kernel_target == KernelTarget.ACCUMULATOR:
+        plt.pcolormesh(fingerprint_times_ms, frequencies, post_kernel)
+        plt.ylabel('Frequency [Hz]')
+        plt.plot(frequency_line, frequencies, 'r-')
+    else: # kernel_target == KernelTarget.DIGEST
+        plt.pcolormesh(fingerprint_times_ms, digest_axis, post_kernel)
+        plt.ylabel('Beat Index')
+        plt.plot(beatindex_line, digest_axis, 'r-')
+    plt.clim(np.percentile(post_kernel[:], 3), np.percentile(post_kernel[:], 97))
+    plt.xlabel('Time [msec]')
+    plt.xticks(time_ticks)
+    plt.title(plot_title)
+    if show_intermediate_plots:
+        plt.show()
+    fig.savefig(os.path.join(plot_dir, f'bias-postkernel-{sanitized_title}.png'))
+    plt.close(fig)
+
     if show_intermediate_plots:
         # Quick and dirty plot of convolution response
-        plt.plot(fingerprint_times, acc_edge_sum)
+        plt.plot(fingerprint_times_ms, post_kernel_flat)
         plt.show()
 
     # Done!
