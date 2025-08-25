a1bb95c31df04e18e90ad2af26dd968ff06f9842
Telperion <twice.invisible@gmail.com>
2025-08-24 22:12:14 -0700

    FFT works!!

--- cpp/src/cli.cpp
+++ cpp/src/cli.cpp
@@ -3,10 +3,76 @@
 
 #include "nine_or_null/nine_or_null.h"
 
+float norm_squared(nine_or_null::CC v) {
+    return (v.real() * v.real() + v.imag() * v.imag());
+}
+
 int main(int argc, const char* argv[]) {
     std::filesystem::path path(argv[0]);
     std::cerr << path.parent_path().string() << std::endl;
     std::cerr << "Hello World! (CLI) " << int(nine_or_null::do_the_thing() * 86400) << std::endl;
+
+    float sample_rate = 44100;
+    float hidden_freq = 3000;
+    int test_size = 6;
+    nine_or_null::Window test_window;
+    nine_or_null::Signal test_signal;
+    nine_or_null::Signal test_fft_result;
+
+    nine_or_null::hann(test_window, test_size);
+    // test_window.assign((2 << test_size) + 1, 1.0f);
+    int test_length = test_window.size();
+
+    for (int i = 0; i < test_length; ++i) {
+        test_signal.push_back(std::sinf(i * hidden_freq * nine_or_null::TAU / sample_rate));
+    }
+
+    nine_or_null::fft(
+        test_fft_result,
+        test_signal,
+        test_window,
+        1 << test_size
+    );
+
+
+    int max_fft_index = 0;
+    float max_fft = 0.0;
+    int graph_height = 40;
+    for (int i = 0; i < test_fft_result.size() / 2; ++i) {
+        auto v = test_fft_result[i];
+        float vv = norm_squared(v);
+        if (vv > max_fft) {
+            max_fft_index = i;
+            max_fft = vv;
+        }
+    }
+
+    char buf[10];
+    for (int i = 0; i < test_fft_result.size(); ++i) {
+        float cr = norm_squared(test_fft_result[i]);
+        int lower = (cr < 0) ? int(graph_height * cr / max_fft) + graph_height : graph_height;
+        int upper = (cr > 0) ? int(graph_height * cr / max_fft) + graph_height : graph_height;
+
+        // float cr = test_signal[i].real();
+        // int lower = (cr < 0) ? int(graph_height * cr) + graph_height : graph_height;
+        // int upper = (cr > 0) ? int(graph_height * cr) + graph_height : graph_height;
+
+        std::snprintf(buf, sizeof(buf), "%4d ", i);
+        std::cerr << buf;
+        for (int i = 0; i < lower; ++i) {
+            std::cerr << "-";
+        }
+        for (int i = lower; i < upper; ++i) {
+            std::cerr << "#";
+        }
+        for (int i = upper; i < 80; ++i) {
+            std::cerr << "-";
+        }
+        std::cerr << std::endl;
+    }
+
+    float principal_freq = (max_fft_index * sample_rate) / (test_length - 1.0f);
+    std::cerr << "Max FFT @ " << principal_freq;
 
     return 0;
 }
--- cpp/src/nine_or_null/nine_or_null.cpp
+++ cpp/src/nine_or_null/nine_or_null.cpp
@@ -1,4 +1,8 @@
+#include <cmath>
+#include <complex>
 #include <ctime>
+#include <iostream>
+#include <vector>
 
 #include "nine_or_null.h"
 
@@ -11,5 +15,91 @@ namespace nine_or_null {
         time_ptr = std::gmtime(&time_raw);
 
         return (time_ptr->tm_hour * 3600.0f + time_ptr->tm_min * 60.0f + time_ptr->tm_sec) / 86400.0f;
+    }
+
+    void hann(Window &window, int size) {
+        int length = (2 << size);
+        window.reserve(length + 1);
+        for (int i = 0; i <= length; ++i) {
+            window.push_back((1.0f - std::cosf((TAU * i) / length)) * 0.5f);
+        }
+    }
+
+    int bit_reversal(int a, int bits) {
+        int b = 0;
+        for (int i = 0; i < bits; i++) {
+            b <<= 1;
+            b += (a & 1);
+            a >>= 1;
+        }
+        return b;
+    }
+
+    void bit_reverser(
+        std::vector<int> &br,
+        int window_length
+    ) {
+        int window_log2 = 0;
+        while ((1 << window_log2) < window_length) {
+            ++window_log2;
+        }
+
+        // Pre-arrange the order for the butterfly transform to pick up the signal values.
+        br.clear();
+        for (int i = 0; i < window_length; i++) {
+            br.push_back(bit_reversal(i, window_log2));
+        }
+        br.push_back(window_length);
+    }
+
+    void fft(
+        Signal &dst,
+        const Signal &src,
+        const Window &window,
+        int center_index,
+        bool invert
+    ) {
+        // How many steps of butterfly transform to perform?
+        int window_length = window.size() - 1;
+        int window_offset = center_index - window_length / 2;
+
+        // Set up the butterfly transform using stride lengths.
+        std::vector<int> reorder(window_length + 1);
+        bit_reverser(reorder, window_length);
+        for (const auto b : reorder) {
+            std::cerr << b << " ";
+        }
+        std::cerr << std::endl;
+
+        dst.clear();
+        dst.reserve(window_length + 1);
+        for (int i = 0; i <= window_length; ++i) {
+            dst.push_back(src[reorder[i] + window_offset] * window[reorder[i]]);
+        }
+
+        for (int stride = 2; stride <= window_length; stride <<= 1) {
+            int half_stride = stride / 2;
+            float theta = (TAU / stride) * (invert ? -1 : 1);
+            CC unity(std::cosf(theta), std::sinf(theta));
+            for (int i = 0; i < window_length; i += stride) {
+                CC winding(1);
+                for (int j = 0; j < half_stride; ++j) {
+                    int butter_index = i + j;
+                    int fly_index = i + j + half_stride;
+                    CC butter(dst[butter_index]);
+                    CC fly(dst[fly_index] * winding);
+                    dst[butter_index] = butter + fly;
+                    dst[fly_index] = butter - fly;
+                    winding *= unity;
+                }
+            }
+        }
+
+        // Scaling when applying the IFFT.
+        if (invert) {
+            for (CC &x : dst) {
+                x /= window_length;
+            }
+        }
     }
 }
